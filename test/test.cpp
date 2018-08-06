73f2bd31485dff0247e8c555bbd9f9293124d52d
Eren Okka <erenokka@gmail.com>
2018-08-06 18:53:46 +0300

    Move to include directory

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2016-2017 Eren Okka
+Copyright (c) 2016-2018 Eren Okka
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -26,7 +26,7 @@ SOFTWARE.
 #include <iostream>
 #include <limits>
 
-#include "../src/semaver.hpp"
+#include "../include/semaver.hpp"
 
 namespace {
 
